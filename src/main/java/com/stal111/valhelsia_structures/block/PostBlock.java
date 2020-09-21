6cbdddce7cd77f3040a14b3d14e41cc4cf393a21
stal111 <stal111@gmx.de>
2020-09-21 20:04:33 +0200

    Posts can now be waterlogged

--- src/main/java/com/stal111/valhelsia_structures/block/PostBlock.java
+++ src/main/java/com/stal111/valhelsia_structures/block/PostBlock.java
@@ -4,23 +4,31 @@ import com.google.common.collect.ImmutableMap;
 import com.stal111.valhelsia_structures.utils.VoxelShapeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.Map;
+import java.util.Objects;
 
-public class PostBlock extends RotatedPillarBlock {
+public class PostBlock extends RotatedPillarBlock implements IWaterLoggable {
 
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final BooleanProperty ATTACHED = BooleanProperty.create("attached");
 
     protected static final Map<Direction.Axis, VoxelShape> SHAPES = ImmutableMap.of(
@@ -30,7 +38,7 @@ public class PostBlock extends RotatedPillarBlock {
 
     public PostBlock(Properties properties) {
         super(properties);
-        this.setDefaultState(this.getStateContainer().getBaseState().with(ATTACHED, false));
+        this.setDefaultState(this.getStateContainer().getBaseState().with(ATTACHED, false).with(WATERLOGGED, false));
     }
 
     @Override
@@ -44,6 +52,22 @@ public class PostBlock extends RotatedPillarBlock {
             world.setBlockState(pos, state.with(ATTACHED, true), 2);
         }
         super.onBlockPlacedBy(world, pos, state, placer, stack);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        FluidState fluidstate = context.getWorld().getFluidState(context.getPos());
+        boolean flag = fluidstate.getFluid() == Fluids.WATER;
+        return Objects.requireNonNull(super.getStateForPlacement(context)).with(WATERLOGGED, flag);
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState state, Direction facing, BlockState facingState, IWorld world, BlockPos currentPos, BlockPos facingPos) {
+        if (state.get(WATERLOGGED)) {
+            world.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.updatePostPlacement(state, facing, facingState, world, currentPos, facingPos);
     }
 
     @Override
@@ -62,7 +86,11 @@ public class PostBlock extends RotatedPillarBlock {
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(ATTACHED);
-        super.fillStateContainer(builder);
+        builder.add(ATTACHED, WATERLOGGED, AXIS);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
     }
 }
