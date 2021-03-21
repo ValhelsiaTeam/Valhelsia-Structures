6a1c7326e9ae3e14cb642ae3dfe2cd8298d9a630
stal111 <stal111@gmx.de>
2021-03-21 12:19:11 +0100

    Update Forge

--- src/main/java/com/stal111/valhelsia_structures/init/ModStructureFeatures.java
+++ src/main/java/com/stal111/valhelsia_structures/init/ModStructureFeatures.java
@@ -1,6 +1,6 @@
 package com.stal111.valhelsia_structures.init;
 
-import com.stal111.valhelsia_structures.world.structures.pieces.*;
+import com.stal111.valhelsia_structures.world.structures.pools.*;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
@@ -18,13 +18,13 @@ import net.minecraft.world.gen.feature.structure.VillageConfig;
 
 public class ModStructureFeatures {
 
-    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> CASTLE = register("castle", ModStructures.CASTLE.get().withConfiguration(new VillageConfig(() -> CastlePools.PATTERN, 7)));
-    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> CASTLE_RUIN = register("castle_ruin", ModStructures.CASTLE_RUIN.get().withConfiguration(new VillageConfig(() -> CastleRuinPools.PATTERN, 7)));
-    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> DESERT_HOUSE = register("desert_house", ModStructures.DESERT_HOUSE.get().withConfiguration(new VillageConfig(() -> DesertHousePools.PATTERN, 7)));
-    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> FORGE = register("forge", ModStructures.FORGE.get().withConfiguration(new VillageConfig(() -> ForgePools.PATTERN, 7)));
-    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> PLAYER_HOUSE = register("player_house", ModStructures.PLAYER_HOUSE.get().withConfiguration(new VillageConfig(() -> PlayerHousePools.PATTERN, 7)));
+    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> CASTLE = register("castle", ModStructures.CASTLE.get().withConfiguration(new VillageConfig(() -> SimpleStructurePools.CASTLE_PATTERN, 7)));
+    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> CASTLE_RUIN = register("castle_ruin", ModStructures.CASTLE_RUIN.get().withConfiguration(new VillageConfig(() -> SimpleStructurePools.CASTLE_RUIN_PATTERN, 7)));
+    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> DESERT_HOUSE = register("desert_house", ModStructures.DESERT_HOUSE.get().withConfiguration(new VillageConfig(() -> SimpleStructurePools.DESERT_HOUSE_PATTERN, 7)));
+    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> FORGE = register("forge", ModStructures.FORGE.get().withConfiguration(new VillageConfig(() -> SimpleStructurePools.FORGE_PATTERN, 7)));
+    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> PLAYER_HOUSE = register("player_house", ModStructures.PLAYER_HOUSE.get().withConfiguration(new VillageConfig(() -> SimpleStructurePools.PLAYER_HOUSE_PATTERN, 7)));
     public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> SMALL_DUNGEON = register("small_dungeon", ModStructures.SMALL_DUNGEON.get().withConfiguration(new VillageConfig(() -> SmallDungeonPools.PATTERN, 7)));
-    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> TOWER_RUIN = register("tower_ruin", ModStructures.TOWER_RUIN.get().withConfiguration(new VillageConfig(() -> TowerRuinPools.PATTERN, 7)));
+    public static final StructureFeature<VillageConfig, ? extends Structure<VillageConfig>> TOWER_RUIN = register("tower_ruin", ModStructures.TOWER_RUIN.get().withConfiguration(new VillageConfig(() -> SimpleStructurePools.TOWER_RUIN_PATTERN, 7)));
 
     private static <FC extends IFeatureConfig, F extends Structure<FC>> StructureFeature<FC, F> register(String name, StructureFeature<FC, F> structureFeature) {
         return WorldGenRegistries.register(WorldGenRegistries.CONFIGURED_STRUCTURE_FEATURE, name, structureFeature);
---
+++ src/main/java/com/stal111/valhelsia_structures/world/structures/pools/SimpleStructurePools.java
@@ -0,0 +1,25 @@
+package com.stal111.valhelsia_structures.world.structures.pools;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import com.stal111.valhelsia_structures.utils.JigsawHelper;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPattern;
+
+/**
+ * Simple Structure Pools
+ * Valhelsia Structures - com.stal111.valhelsia_structures.world.structures.pools.SimpleStructurePools
+ *
+ * @author Valhelsia Team
+ * @version 16.1.0
+ * @since 2021-03-21
+ */
+public class SimpleStructurePools {
+
+    public static final JigsawPattern CASTLE_PATTERN = JigsawHelper.register("castles", JigsawPattern.PlacementBehaviour.RIGID, ImmutableList.of(Pair.of("castle", 1)), true);
+    public static final JigsawPattern CASTLE_RUIN_PATTERN = JigsawHelper.register("castle_ruins", JigsawPattern.PlacementBehaviour.RIGID, ImmutableList.of(Pair.of("castle_ruin", 1)), true);
+    public static final JigsawPattern DESERT_HOUSE_PATTERN = JigsawHelper.register("desert_houses", JigsawPattern.PlacementBehaviour.RIGID, ImmutableList.of(Pair.of("desert_house", 1)));
+    public static final JigsawPattern FORGE_PATTERN = JigsawHelper.register("forges", JigsawPattern.PlacementBehaviour.RIGID, ImmutableList.of(Pair.of("forge", 1)), true);
+    public static final JigsawPattern PLAYER_HOUSE_PATTERN = JigsawHelper.register("player_houses", JigsawPattern.PlacementBehaviour.RIGID, ImmutableList.of(Pair.of("player_house", 1)), true);
+    public static final JigsawPattern TOWER_RUIN_PATTERN = JigsawHelper.register("tower_ruins", JigsawPattern.PlacementBehaviour.RIGID, ImmutableList.of(Pair.of("tower_ruin", 1), Pair.of("tower_ruin1", 1), Pair.of("tower_ruin2", 1)), true);
+
+}
