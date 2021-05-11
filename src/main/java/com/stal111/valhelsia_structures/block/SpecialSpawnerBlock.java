e972ba12d65f277deafbc17d1fb994369ad9a1e0
stal111 <stal111@gmx.de>
2021-05-11 14:04:53 +0200

    Cleanup SpecialSpawnerBlock.java

--- src/main/java/com/stal111/valhelsia_structures/block/SpecialSpawnerBlock.java
+++ src/main/java/com/stal111/valhelsia_structures/block/SpecialSpawnerBlock.java
@@ -3,8 +3,6 @@ package com.stal111.valhelsia_structures.block;
 import com.stal111.valhelsia_structures.tileentity.SpecialMobSpawnerTileEntity;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.ContainerBlock;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
@@ -13,52 +11,67 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.valhelsia.valhelsia_core.block.ValhelsiaContainerBlock;
 
-public class SpecialSpawnerBlock extends ContainerBlock {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Special Spawner Block
+ * Valhelsia Structures - com.stal111.valhelsia_structures.block.SpecialSpawnerBlock
+ *
+ * @author Valhelsia Team
+ * @version 16.1.0
+ */
+public class SpecialSpawnerBlock extends ValhelsiaContainerBlock {
 
     public SpecialSpawnerBlock(Properties properties) {
         super(properties);
     }
 
-    public TileEntity createNewTileEntity(IBlockReader worldIn) {
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new SpecialMobSpawnerTileEntity();
     }
 
-    public void spawnAdditionalDrops(BlockState state, ServerWorld world, BlockPos pos, ItemStack stack) {
-        super.spawnAdditionalDrops(state, world, pos, stack);
-    }
-
     @Override
-    public int getExpDrop(BlockState state, net.minecraft.world.IWorldReader world, BlockPos pos, int fortune, int silktouch) {
+    public int getExpDrop(BlockState state, IWorldReader world, BlockPos pos, int fortune, int silktouch) {
         return 10 + RANDOM.nextInt(15) + RANDOM.nextInt(15);
     }
 
+    @Nonnull
     @Override
-    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult result) {
+    public ActionResultType onBlockActivated(@Nonnull BlockState state, World world, @Nonnull BlockPos pos, PlayerEntity player, @Nonnull Hand hand, @Nonnull BlockRayTraceResult result) {
         ItemStack stack = player.getHeldItemMainhand();
-        TileEntity tileentity = world.getTileEntity(pos);
-        if (stack.getItem() instanceof SpawnEggItem) {
-            if (tileentity instanceof SpecialMobSpawnerTileEntity) {
-                SpecialAbstractSpawner abstractspawner = ((SpecialMobSpawnerTileEntity)tileentity).getSpawnerBaseLogic();
-                EntityType<?> entitytype1 = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
-                abstractspawner.setEntityType(entitytype1);
-                tileentity.markDirty();
-                world.notifyBlockUpdate(pos, state, state, 3);
-                stack.shrink(1);
-                return ActionResultType.SUCCESS;
-            }
+        TileEntity tileEntity = world.getTileEntity(pos);
+
+        if (tileEntity == null || !(stack.getItem() instanceof SpawnEggItem)) {
+            return ActionResultType.PASS;
         }
-        return super.onBlockActivated(state, world, pos, player, hand, result);
+        SpecialAbstractSpawner spawner = ((SpecialMobSpawnerTileEntity) tileEntity).getSpawnerBaseLogic();
+
+        spawner.setEntityType(((SpawnEggItem) stack.getItem()).getType(stack.getTag()));
+        tileEntity.markDirty();
+        world.notifyBlockUpdate(pos, state, state, 3);
+
+        if (!player.abilities.isCreativeMode) {
+            stack.shrink(1);
+        }
+
+        return ActionResultType.func_233537_a_(world.isRemote());
     }
 
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
     }
 
-    public ItemStack getItem(IBlockReader worldIn, BlockPos pos, BlockState state) {
+    @Override
+    public ItemStack getPickBlock(BlockState state, RayTraceResult target, IBlockReader world, BlockPos pos, PlayerEntity player) {
         return ItemStack.EMPTY;
     }
 }
