dc80f28a225ab7b8d7e3b27ace7934a38d102e17
stal111 <stal111@gmx.de>
2021-05-15 15:01:45 +0200

    Fix Pillagers spawning without crossbow

--- src/main/java/com/stal111/valhelsia_structures/mixin/PillagerEntityMixin.java
+++ src/main/java/com/stal111/valhelsia_structures/mixin/PillagerEntityMixin.java
@@ -3,7 +3,6 @@ package com.stal111.valhelsia_structures.mixin;
 import net.minecraft.entity.monster.PillagerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -20,9 +19,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class PillagerEntityMixin {
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/monster/PillagerEntity;setItemStackToSlot(Lnet/minecraft/inventory/EquipmentSlotType;Lnet/minecraft/item/ItemStack;)V"), method = "setEquipmentBasedOnDifficulty")
-    private void valhelsia_avoidOverridingEquipment(PillagerEntity pillagerEntity, EquipmentSlotType slotIn, ItemStack stack) {
-        if (stack.isEmpty()) {
-            pillagerEntity.setItemStackToSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.CROSSBOW));
+    private void valhelsia_avoidOverridingEquipment(PillagerEntity entity, EquipmentSlotType slot, ItemStack stack) {
+        if (!entity.hasItemInSlot(slot)) {
+            entity.setItemStackToSlot(slot, stack);
         }
     }
 }
