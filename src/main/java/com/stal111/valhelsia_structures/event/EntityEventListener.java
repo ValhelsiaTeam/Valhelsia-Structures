f06e9a09adffa13ace6592f997bdefc0a422dae3
stal111 <stal111@gmx.de>
2021-06-01 20:36:38 +0200

    Add MeleeAttackGoal to Pillagers

--- src/main/java/com/stal111/valhelsia_structures/event/EntityEventListener.java
+++ src/main/java/com/stal111/valhelsia_structures/event/EntityEventListener.java
@@ -2,8 +2,12 @@ package com.stal111.valhelsia_structures.event;
 
 import com.stal111.valhelsia_structures.ValhelsiaStructures;
 import com.stal111.valhelsia_structures.init.ModEntities;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.monster.AbstractSkeletonEntity;
+import net.minecraft.entity.monster.PillagerEntity;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,14 +18,27 @@ import net.minecraftforge.fml.common.Mod;
  * For all entity-related event handling.
  * </p>
  * @author Valhelsia Team
- * @version 16.1.0
+ * @version 1.0.2
  * @since 2021-05-11
  */
-
-@Mod.EventBusSubscriber(modid = ValhelsiaStructures.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = ValhelsiaStructures.MOD_ID)
 public class EntityEventListener {
+
     @SubscribeEvent
-    public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
-        event.put(ModEntities.MOSSY_SKELETON.get(), AbstractSkeletonEntity.registerAttributes().create());
+    public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof PillagerEntity && !event.getWorld().isRemote()) {
+            PillagerEntity pillagerEntity = (PillagerEntity) entity;
+            pillagerEntity.goalSelector.addGoal(5, new MeleeAttackGoal(pillagerEntity, 1.0D, true));
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = ValhelsiaStructures.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ModEvents {
+
+        @SubscribeEvent
+        public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+            event.put(ModEntities.MOSSY_SKELETON.get(), AbstractSkeletonEntity.registerAttributes().create());
+        }
     }
 }
