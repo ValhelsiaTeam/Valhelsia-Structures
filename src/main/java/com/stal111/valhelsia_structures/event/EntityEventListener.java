e9a3bf970e5409e8813a3a1bbe6ae9123984afb5
stal111 <stal111@gmx.de>
2021-08-07 16:48:05 +0200

    Add custom explorer maps to cartographer

--- src/main/java/com/stal111/valhelsia_structures/event/EntityEventListener.java
+++ src/main/java/com/stal111/valhelsia_structures/event/EntityEventListener.java
@@ -2,23 +2,32 @@ package com.stal111.valhelsia_structures.event;
 
 import com.stal111.valhelsia_structures.ValhelsiaStructures;
 import com.stal111.valhelsia_structures.init.ModEntities;
+import com.stal111.valhelsia_structures.init.ModStructures;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.entity.monster.AbstractSkeletonEntity;
 import net.minecraft.entity.monster.PillagerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MerchantOffer;
+import net.minecraft.world.storage.MapDecoration;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.valhelsia.valhelsia_core.helper.TradeHelper;
+import net.valhelsia.valhelsia_core.util.ItemStackUtils;
 
 /**
- * Entity Event Listener
+ * Entity Event Listener <br>
  * Valhelsia Structures - com.stal111.valhelsia_structures.event.EntityEventListener
  * <p>
  * For all entity-related event handling.
  * </p>
  * @author Valhelsia Team
- * @version 1.0.2
+ * @version 0.1.5
  * @since 2021-05-11
  */
 @Mod.EventBusSubscriber(modid = ValhelsiaStructures.MOD_ID)
@@ -31,6 +40,32 @@ public class EntityEventListener {
             PillagerEntity pillagerEntity = (PillagerEntity) entity;
             pillagerEntity.goalSelector.addGoal(5, new MeleeAttackGoal(pillagerEntity, 1.0D, true));
         }
+    }
+
+    @SubscribeEvent
+    public static void onVillagerTrade(VillagerTradesEvent event) {
+        TradeHelper.addVillagerTrade(event,
+                VillagerProfession.CARTOGRAPHER,
+                1,
+                (trader, rand) -> {
+                    ItemStack stack = ItemStackUtils.getFilledMap(trader.world, trader.getPosition(), ModStructures.SPAWNER_DUNGEON.get(), MapDecoration.Type.RED_X);
+                    if (stack == null) {
+                        return null;
+                    }
+                    return new MerchantOffer(new ItemStack(Items.EMERALD, 10), new ItemStack(Items.COMPASS), stack, 12, 5, 1);
+                }
+        );
+        TradeHelper.addVillagerTrade(event,
+                VillagerProfession.CARTOGRAPHER,
+                2,
+                (trader, rand) -> {
+                    ItemStack stack = ItemStackUtils.getFilledMap(trader.world, trader.getPosition(), ModStructures.CASTLE.get(), MapDecoration.Type.RED_X);
+                    if (stack == null) {
+                        return null;
+                    }
+                    return new MerchantOffer(new ItemStack(Items.EMERALD, 12), new ItemStack(Items.COMPASS), stack, 12, 10, 1);
+                }
+        );
     }
 
     @Mod.EventBusSubscriber(modid = ValhelsiaStructures.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
