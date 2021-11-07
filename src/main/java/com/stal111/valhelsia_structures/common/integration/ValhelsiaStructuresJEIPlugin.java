12a14e4dd64b53506a4ebe735830298c309ed90f
stal111 <stal111@gmx.de>
2021-11-07 12:13:35 +0100

    Hide unreleased blocks

--- src/main/java/com/stal111/valhelsia_structures/common/integration/ValhelsiaStructuresJEIPlugin.java
+++ src/main/java/com/stal111/valhelsia_structures/common/integration/ValhelsiaStructuresJEIPlugin.java
@@ -1,10 +1,13 @@
 package com.stal111.valhelsia_structures.common.integration;
 
+import com.stal111.valhelsia_structures.common.item.ModCreativeModeTabs;
 import com.stal111.valhelsia_structures.core.ValhelsiaStructures;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IJeiRuntime;
 import net.minecraft.resources.ResourceLocation;
 
 import javax.annotation.Nonnull;
@@ -26,6 +29,11 @@ public class ValhelsiaStructuresJEIPlugin implements IModPlugin {
     @Nonnull
     public ResourceLocation getPluginUid() {
         return ID;
+    }
+
+    @Override
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+        jeiRuntime.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM, ModCreativeModeTabs.HIDDEN_ITEMS);
     }
 
     @Override
--- src/main/java/com/stal111/valhelsia_structures/common/item/ModCreativeModeTabs.java
+++ src/main/java/com/stal111/valhelsia_structures/common/item/ModCreativeModeTabs.java
@@ -2,10 +2,15 @@ package com.stal111.valhelsia_structures.common.item;
 
 import com.stal111.valhelsia_structures.core.ValhelsiaStructures;
 import com.stal111.valhelsia_structures.core.init.ModBlocks;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Mod Creative Mode Tabs <br>
@@ -16,12 +21,31 @@ import javax.annotation.Nonnull;
  * @since 2020-06-01
  */
 public class ModCreativeModeTabs {
+
+    public static final List<ItemStack> HIDDEN_ITEMS = new ArrayList<>();
+
     public static final CreativeModeTab MAIN = new CreativeModeTab(ValhelsiaStructures.MOD_ID + "_main") {
 
         @Nonnull
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModBlocks.BRAZIER.get().asItem());
+        }
+
+        @Override
+        public void fillItemList(@Nonnull NonNullList<ItemStack> items) {
+            super.fillItemList(items);
+            HIDDEN_ITEMS.clear();
+
+            items.removeIf(itemStack -> {
+                Item item = itemStack.getItem();
+
+                if (item.getRegistryName().toString().contains("lapidified") || (item instanceof BlockItem blockItem && (blockItem.getBlock() == ModBlocks.EXPLORERS_TENT.get() || blockItem.getBlock() == ModBlocks.GIANT_FERN.get() || blockItem.getBlock() == ModBlocks.HIBISCUS.get()))) {
+                    HIDDEN_ITEMS.add(itemStack);
+                    return true;
+                }
+                return false;
+            });
         }
     };
 }
