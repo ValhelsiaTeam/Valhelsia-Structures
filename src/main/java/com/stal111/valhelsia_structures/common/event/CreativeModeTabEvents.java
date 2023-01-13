880a3aeb104c24c63e6baf02d1f0031b88f45c1a
stal111 <stal111@gmx.de>
2023-01-13 21:15:11 +0100

    Hide certain items

--- src/main/java/com/stal111/valhelsia_structures/common/event/CreativeModeTabEvents.java
+++ src/main/java/com/stal111/valhelsia_structures/common/event/CreativeModeTabEvents.java
@@ -1,12 +1,18 @@
 package com.stal111.valhelsia_structures.common.event;
 
+import com.google.common.collect.ImmutableList;
 import com.stal111.valhelsia_structures.core.ValhelsiaStructures;
 import com.stal111.valhelsia_structures.core.init.ModBlocks;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author Valhelsia Team
@@ -14,14 +20,29 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
  */
 public class CreativeModeTabEvents {
 
+    public static final ImmutableList<Supplier<? extends ItemLike>> HIDDEN_ITEMS = ImmutableList.<Supplier<? extends ItemLike>>builder()
+            .add(ModBlocks.WOODEN_POSTS.get(ModBlocks.WoodType.LAPIDIFIED_JUNGLE))
+            .add(ModBlocks.STRIPPED_WOODEN_POSTS.get(ModBlocks.WoodType.LAPIDIFIED_JUNGLE))
+            .add(ModBlocks.CUT_WOODEN_POSTS.get(ModBlocks.WoodType.LAPIDIFIED_JUNGLE))
+            .add(ModBlocks.CUT_STRIPPED_WOODEN_POSTS.get(ModBlocks.WoodType.LAPIDIFIED_JUNGLE))
+            .add(ModBlocks.BUNDLED_POSTS.get(ModBlocks.WoodType.LAPIDIFIED_JUNGLE))
+            .add(ModBlocks.BUNDLED_STRIPPED_POSTS.get(ModBlocks.WoodType.LAPIDIFIED_JUNGLE))
+            .add(ModBlocks.HIBISCUS)
+            .add(ModBlocks.GIANT_FERN)
+            .build();
+
     @SubscribeEvent
     public void registerTabs(CreativeModeTabEvent.Register event) {
         event.registerCreativeModeTab(new ResourceLocation(ValhelsiaStructures.MOD_ID, "main"), builder -> builder
                 .icon(() -> new ItemStack(ModBlocks.BRAZIER.get()))
                 .title(Component.translatable("itemGroup.valhelsia_structures"))
                 .displayItems((featureFlags, output, hasOp) -> {
+                    List<Item> hiddenItems = HIDDEN_ITEMS.stream().map(supplier -> supplier.get().asItem()).toList();
+
                     ValhelsiaStructures.REGISTRY_MANAGER.getItemHelper().getRegistryObjects().forEach(registryObject -> {
-                        output.accept(registryObject.get());
+                        if (!hiddenItems.contains(registryObject.get())) {
+                            output.accept(registryObject.get());
+                        }
                     });
                 })
         );
