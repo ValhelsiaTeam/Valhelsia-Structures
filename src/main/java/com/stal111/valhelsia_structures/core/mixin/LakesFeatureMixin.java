d0db5264174f00cba2110498b2e0fbad08c70552
stal111 <stal111@gmx.de>
2023-01-22 16:09:33 +0100

    Update LakesFeatureMixin.java

--- src/main/java/com/stal111/valhelsia_structures/core/mixin/LakesFeatureMixin.java
+++ src/main/java/com/stal111/valhelsia_structures/core/mixin/LakesFeatureMixin.java
@@ -1,6 +1,9 @@
 package com.stal111.valhelsia_structures.core.mixin;
 
+import com.stal111.valhelsia_structures.common.world.structures.ValhelsiaJigsawStructure;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.LakeFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.BlockStateConfiguration;
@@ -14,7 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  * Valhelsia Structures - com.stal111.valhelsia_structures.core.mixin.LakesFeatureMixin
  *
  * @author Valhelsia Team
- * @version 1.19 - 0.2.0
  * @since 2021-02-08
  */
 @Mixin(LakeFeature.class)
@@ -23,17 +25,11 @@ public class LakesFeatureMixin {
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;below(I)Lnet/minecraft/core/BlockPos;"), method = "place", cancellable = true)
     private void valhelsia_checkForStructures(FeaturePlaceContext<BlockStateConfiguration> context, CallbackInfoReturnable<Boolean> cir) {
         if (context.level() instanceof ServerLevel serverLevel) {
-//            for (RegistryObject<Structure> registryObject : ModStructures.HELPER.getRegistryObjects()) {
-//                Structure structure = registryObject.get();
-//
-//                if (structure.step() != GenerationStep.Decoration.SURFACE_STRUCTURES) {
-//                    continue;
-//                }
-//
-//                if (!serverLevel.structureManager().startsForStructure(SectionPos.of(context.origin()), structure).isEmpty()) {
-//                    cir.setReturnValue(false);
-//                }
-//            }
+            if (!serverLevel.structureManager().startsForStructure(new ChunkPos(context.origin()), structure -> {
+                return structure instanceof ValhelsiaJigsawStructure && structure.step() == GenerationStep.Decoration.SURFACE_STRUCTURES;
+            }).isEmpty()) {
+                cir.setReturnValue(false);
+            }
         }
     }
 }
