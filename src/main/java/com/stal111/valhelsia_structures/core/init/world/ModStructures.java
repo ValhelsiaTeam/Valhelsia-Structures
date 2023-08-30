1499da4e9559617c889d9aec4b1929c2b7952675
stal111 <stal111@gmx.de>
2023-08-30 17:05:02 +0200

    Slightly lower structure generation chance

--- src/main/java/com/stal111/valhelsia_structures/core/init/world/ModStructures.java
+++ src/main/java/com/stal111/valhelsia_structures/core/init/world/ModStructures.java
@@ -68,18 +68,18 @@ public class ModStructures extends DatapackRegistryClass<Structure> {
         HolderSet<Biome> spawnerRoomBiomes = this.singleTag(biomeHolderGetter, ModTags.Biomes.HAS_SPAWNER_ROOM);
         HolderSet<Biome> deepSpawnerRoomBiomes = this.singleTag(biomeHolderGetter, ModTags.Biomes.HAS_DEEP_SPAWNER_ROOM);
 
-        this.surfaceStructure(context, CASTLE, castleBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.CASTLES, builder -> builder.chance(0.5D));
-        this.surfaceStructure(context, CASTLE_RUIN, castleRuinBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.CASTLE_RUINS, builder -> builder.chance(0.6D));
-        this.surfaceStructure(context, DESERT_HOUSE, desertHouseBiomes, TerrainAdjustment.BEARD_THIN, DesertHousePools.START, builder -> builder.chance(0.75D));
-        this.surfaceStructure(context, FORGE, forgeBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.FORGES, builder -> builder.chance(0.75D));
-        this.surfaceStructure(context, PLAYER_HOUSE, playerHouseBiomes, TerrainAdjustment.BEARD_THIN, PlayerHousePools.START, builder -> builder.chance(0.75D));
-        this.surfaceStructure(context, SPAWNER_DUNGEON, spawnerDungeonBiomes, TerrainAdjustment.NONE, SpawnerDungeonPools.START, builder -> builder.chance(0.8D).startHeight(StructureHeightProvider.surfaceBetween(VerticalAnchor.absolute(0), VerticalAnchor.absolute(75))).individualTerrainAdjustment());
-        this.surfaceStructure(context, TOWER_RUIN, towerRuinBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.TOWER_RUINS, builder -> builder.chance(0.8D));
+        this.surfaceStructure(context, CASTLE, castleBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.CASTLES, builder -> builder.chance(0.4D));
+        this.surfaceStructure(context, CASTLE_RUIN, castleRuinBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.CASTLE_RUINS, builder -> builder.chance(0.5D));
+        this.surfaceStructure(context, DESERT_HOUSE, desertHouseBiomes, TerrainAdjustment.BEARD_THIN, DesertHousePools.START, builder -> builder.chance(0.7D));
+        this.surfaceStructure(context, FORGE, forgeBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.FORGES, builder -> builder.chance(0.65D));
+        this.surfaceStructure(context, PLAYER_HOUSE, playerHouseBiomes, TerrainAdjustment.BEARD_THIN, PlayerHousePools.START, builder -> builder.chance(0.65D));
+        this.surfaceStructure(context, SPAWNER_DUNGEON, spawnerDungeonBiomes, TerrainAdjustment.NONE, SpawnerDungeonPools.START, builder -> builder.chance(0.7D).startHeight(StructureHeightProvider.surfaceBetween(VerticalAnchor.absolute(0), VerticalAnchor.absolute(75))).individualTerrainAdjustment());
+        this.surfaceStructure(context, TOWER_RUIN, towerRuinBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.TOWER_RUINS, builder -> builder.chance(0.7D));
         this.surfaceStructure(context, WITCH_HUT, witchHutBiomes, TerrainAdjustment.BEARD_THIN, SimpleStructurePools.WITCH_HUTS, builder -> builder.chance(0.85D).margin(3)
                 .addSpawnOverride(MobCategory.MONSTER, new StructureSpawnOverride(StructureSpawnOverride.BoundingBoxType.PIECE, WeightedRandomList.create(new MobSpawnSettings.SpawnerData(EntityType.WITCH, 1, 1, 1))))
                 .addSpawnOverride(MobCategory.CREATURE, new StructureSpawnOverride(StructureSpawnOverride.BoundingBoxType.PIECE, WeightedRandomList.create(new MobSpawnSettings.SpawnerData(EntityType.CAT, 1, 1, 1))))
         );
-        this.surfaceStructure(context, BIG_TREE, bigTreeBiomes, TerrainAdjustment.BEARD_THIN, BigTreePools.START, builder -> builder.chance(0.7D));
+        this.surfaceStructure(context, BIG_TREE, bigTreeBiomes, TerrainAdjustment.BEARD_THIN, BigTreePools.START, builder -> builder.chance(0.6D));
 
         this.undergroundStructure(context, SPAWNER_ROOM, spawnerRoomBiomes, TerrainAdjustment.NONE, SimpleStructurePools.SPAWNER_ROOMS, builder -> builder.chance(0.9D).startHeight(StructureHeightProvider.spawnerRoom(VerticalAnchor.absolute(0))));
         this.undergroundStructure(context, DEEP_SPAWNER_ROOM, deepSpawnerRoomBiomes, TerrainAdjustment.NONE, SimpleStructurePools.DEEP_SPAWNER_ROOMS, builder -> builder.startHeight(StructureHeightProvider.deepSpawnerRoom(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(-1))));
