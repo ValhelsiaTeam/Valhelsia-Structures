693320dd8f3de1f5dab18de4d2703f2e9945089e
stal111 <stal111@gmx.de>
2025-10-05 12:41:56 +0200

    Update BeardifierMixin

--- neoforge/src/main/java/com/stal111/valhelsia_structures/core/mixin/BeardifierMixin.java
+++ neoforge/src/main/java/com/stal111/valhelsia_structures/core/mixin/BeardifierMixin.java
@@ -1,7 +1,15 @@
 package com.stal111.valhelsia_structures.core.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.stal111.valhelsia_structures.common.world.structures.ValhelsiaJigsawStructure;
+import com.stal111.valhelsia_structures.common.world.structures.pools.ValhelsiaPoolElementWrapper;
 import net.minecraft.world.level.levelgen.Beardifier;
+import net.minecraft.world.level.levelgen.structure.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 /**
  * @author Valhelsia Team
@@ -10,43 +18,27 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(Beardifier.class)
 public class BeardifierMixin {
 
-//    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/Structure;terrainAdaptation()Lnet/minecraft/world/level/levelgen/structure/TerrainAdjustment;"), method = {"lambda$forStructuresInChunk$1", "m_223940_"})
-//    private static TerrainAdjustment valhelsia_forStructuresInChunk(Structure structure) {
-//        if (structure instanceof ValhelsiaJigsawStructure valhelsiaJigsawStructure && valhelsiaJigsawStructure.hasIndividualTerrainAdjustment()) {
-//            return TerrainAdjustment.BEARD_THIN;
-//        }
-//
-//        return structure.terrainAdaptation();
-//    }
-//
-//    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/StructureStart;getStructure()Lnet/minecraft/world/level/levelgen/structure/Structure;"), method = "forStructuresInChunk", cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
-//    private static void valhelsia_forStructuresInChunk(StructureManager structureManager, ChunkPos chunkPos, CallbackInfoReturnable<Beardifier> cir, List list, int i, int j, List list1, List list2, BoundingBox boundingbox, Iterator var8, StructureStart structurestart, ChunkPos chunkPos2, ObjectList<Beardifier.Rigid> rigids, int minX, int minZ, ObjectList<JigsawJunction> junctions, StructureStart start) {
-//        if (start.getStructure() instanceof ValhelsiaJigsawStructure valhelsiaJigsawStructure && valhelsiaJigsawStructure.hasIndividualTerrainAdjustment()) {
-//            TerrainAdjustment terrainAdjustment = start.getStructure().terrainAdaptation();
-//
-//            for(StructurePiece structurePiece : start.getPieces()) {
-//                if (structurePiece.isCloseToChunk(chunkPos, 12)) {
-//                    if (structurePiece instanceof PoolElementStructurePiece piece) {
-//                        StructureTemplatePool.Projection projection = piece.getElement().getProjection();
-//                        if (projection == StructureTemplatePool.Projection.RIGID) {
-//                            terrainAdjustment = (piece.getElement() instanceof ValhelsiaPoolElementWrapper wrapper && wrapper.getTerrainAdjustment() != null) ? wrapper.getTerrainAdjustment() : terrainAdjustment;
-//                            rigids.add(new Beardifier.Rigid(piece.getBoundingBox(), terrainAdjustment, piece.getGroundLevelDelta()));
-//                        }
-//
-//                        for(JigsawJunction jigsawjunction : piece.getJunctions()) {
-//                            int k = jigsawjunction.getSourceX();
-//                            int l = jigsawjunction.getSourceZ();
-//                            if (k > minX - 12 && l > minZ - 12 && k < minX + 15 + 12 && l < minZ + 15 + 12) {
-//                                junctions.add(jigsawjunction);
-//                            }
-//                        }
-//                    } else {
-//                        rigids.add(new Beardifier.Rigid(structurePiece.getBoundingBox(), terrainAdjustment, 0));
-//                    }
-//                }
-//            }
-//
-//            cir.cancel();
-//        }
-//    }
+    @WrapOperation(
+            method = "lambda$forStructuresInChunk$1",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/Structure;terrainAdaptation()Lnet/minecraft/world/level/levelgen/structure/TerrainAdjustment;"))
+    private static TerrainAdjustment valhelsia_forStructuresInChunk(Structure structure, Operation<TerrainAdjustment> original) {
+        if (structure instanceof ValhelsiaJigsawStructure valhelsiaJigsawStructure && valhelsiaJigsawStructure.hasIndividualTerrainAdjustment()) {
+            return TerrainAdjustment.BEARD_THIN;
+        }
+
+        return original.call(structure);
+    }
+
+    @ModifyArg(
+            method = "forStructuresInChunk",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 1))
+    private static <E> E valhelsia_forStructuresInChunk(E e, @Local StructureStart start, @Local StructurePiece structurePiece) {
+        if (start.getStructure() instanceof ValhelsiaJigsawStructure valhelsiaJigsawStructure && valhelsiaJigsawStructure.hasIndividualTerrainAdjustment()) {
+            if (e instanceof Beardifier.Rigid rigid && structurePiece instanceof PoolElementStructurePiece piece && piece.getElement() instanceof ValhelsiaPoolElementWrapper wrapper && wrapper.getTerrainAdjustment() != null) {
+                return (E) new Beardifier.Rigid(rigid.box(), wrapper.getTerrainAdjustment(), rigid.groundLevelDelta());
+            }
+        }
+
+        return e;
+    }
 }
